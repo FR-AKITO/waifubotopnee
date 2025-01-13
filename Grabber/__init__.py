a802efce392d9aaae42f6af3f3f6eba55d023768
Aᴋɪᴛᴏ <varshakoli638@gmail.com>
2025-01-13 18:49:12 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -16,16 +16,16 @@ logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-OWNER_ID = "6632519077"
-sudo_users = ["6632519077"]
-GROUP_ID = "-1002055176072"
-TOKEN = "7256217879:AAEM3lP4dTxN-3LJDvpXuik-PeFcY8n4oyE"
+OWNER_ID = "1381668733"
+sudo_users = ["1381668733"]
+GROUP_ID = "-1002383819050"
+TOKEN = "7567555779:AAFA7hx8Q-orYico0ojGrp6V80pRFXiFl6g"
 mongo_url = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
 PHOTO_URL = ["https://telegra.ph/file/a17bbdf36197b0f0eb2c1.jpg", "https://telegra.ph/file/4754711cd88be32baf5b4.jpg", "https://telegra.ph/file/46b1151c6088fabc62250.jpg", "https://telegra.ph/file/4ed692d4e678216f87083.jpg"]
-SUPPORT_CHAT = "BeybladeVerse"
-UPDATE_CHAT = "BeybladeXD"
-BOT_USERNAME = "YxH_Game_Bot"
-CHARA_CHANNEL_ID = "-1002055176072"
+SUPPORT_CHAT = "Asia_Anime_Community"
+UPDATE_CHAT = "Anime_Asia_Community"
+BOT_USERNAME = "Son_Goku_ro_bot"
+CHARA_CHANNEL_ID = "-1002383819050"
 api_id = "20457610"
 api_hash = "b7de0dfecd19375d3f84dbedaeb92537"
 
